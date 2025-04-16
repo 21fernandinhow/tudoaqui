a8b8f8dcbc0f5755e0f0d92a2ee4e4dd525e3031
Fernando Carvalho <21fernandinhow@gmail.com>
2025-04-16 19:51:38 -0300

    PLEASE I JUST WANT IT TO WORK

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
 import { UserDataProvider } from "./context/UserDataContext";
 import { AppRoutes } from "./AppRoutes"
 import "./styles/index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <UserDataProvider>
-        <BrowserRouter basename="/">
-          <AppRoutes />
-        </BrowserRouter>
-      </UserDataProvider>
-    </>
+    <UserDataProvider>
+      <BrowserRouter basename='/'>
+        <AppRoutes />
+      </BrowserRouter>
+    </UserDataProvider>
   );
 }
 
