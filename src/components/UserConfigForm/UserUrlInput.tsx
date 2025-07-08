425e8c246f8afdd241fa4abd1dd116d07dd98ff5
Fernando Carvalho <21fernandinhow@gmail.com>
2025-07-08 10:36:33 -0300

    improve url input

--- src/components/UserConfigForm/UserUrlInput.tsx
+++ src/components/UserConfigForm/UserUrlInput.tsx
@@ -5,13 +5,25 @@ interface UserUrlInputProps {
 
 export const UserUrlInput = ({ userUrl, updateUserUrl }: UserUrlInputProps) => {
 
+    const slugify = (input: string) => {
+        return input
+            .normalize('NFD')                     // Remove acentos
+            .replace(/[\u0300-\u036f]/g, '')      // Remove marcas de acento
+            .toLowerCase()
+            .trim()
+            .replace(/\s+/g, '-')                 // Substitui espaços por hífens
+            .replace(/[^a-z0-9-]/g, '')           // Remove tudo que não é letra, número ou hífen
+            .replace(/--+/g, '-')                 // Evita múltiplos hífens seguidos
+            .slice(0, 40);
+    }
+
     return (
         <>
             <h4>Sua URL</h4>
             <input
                 id="url-input"
                 type="text"
-                onChange={(e) => updateUserUrl("userUrl", e.target.value.replace(/\s+/g, '').slice(0, 40))}
+                onChange={(e) => updateUserUrl("userUrl", slugify(e.target.value))}
                 value={userUrl}
                 placeholder="Digite aqui..."
             />
