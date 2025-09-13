96c71da5e24417ffbfc84b7860e7f31b59f57543
Fernando Carvalho <21fernandinhow@gmail.com>
2025-09-13 11:57:36 -0300

    social icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+
 export const Footer = () => (
     <footer>
-        Copyright tudoaqui 2025.
+        <p>Copyright tudoaqui 2025.</p>
+        <div className="social-icons">
+            <a href="https://www.instagram.com/tudoaqui.click/" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+            </a>
+            <a href="https://x.com/tudoaquiclick" target="_blank" rel="noopener noreferrer">
+                <FaXTwitter />
+            </a>
+        </div>
     </footer>
 )
