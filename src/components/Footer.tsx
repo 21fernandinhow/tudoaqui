eccffe1e5639b410fbff05f1ff5d5177a47c7328
Fernando Carvalho <21fernandinhow@gmail.com>
2025-12-09 10:27:48 -0300

    rewrite footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,1 @@
-import { FaInstagram, FaXTwitter } from "react-icons/fa6";
-
-export const Footer = () => (
-    <footer>
-        <p>Copyright tudoaqui 2025.</p>
-        <div className="social-icons">
-            <a href="https://www.instagram.com/tudoaqui.click/" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
-            </a>
-            <a href="https://x.com/tudoaquiclick" target="_blank" rel="noopener noreferrer">
-                <FaXTwitter />
-            </a>
-        </div>
-    </footer>
-)
+export const Footer = () => <footer> <p>Copyright tudoaqui 2025. Feito no Brasil 🇧🇷</p> </footer>
