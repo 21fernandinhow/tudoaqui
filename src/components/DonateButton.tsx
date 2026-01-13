bb358e47ae6a3f3ef5fb1e8fb9746bbb5ca68654
Fernando Carvalho <21fernandinhow@gmail.com>
2026-01-13 12:39:11 -0300

    upgrade footer and donate modal logic

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -1,21 +1,10 @@
-import { BiSolidDonateHeart } from "react-icons/bi";
-import { Modal } from "./Modal";
 import { useState } from "react";
-import DonateQRCode from "../assets/donate-qrcode.png";
-import { useSnackbar } from "../contexts/SnackbarContext";
+import { DonateModal } from "./DonateModal";
+import { BiSolidDonateHeart } from "react-icons/bi";
 
 export const DonateButton = () => {
 
-    const { showSnackbar } = useSnackbar();
     const [donateModalIsOpen, setDonateModalIsOpen] = useState(false);
-
-    const pixCopyPaste =
-        "00020126580014BR.GOV.BCB.PIX013648f4b53c-7dfb-438a-bbb9-0afb81850c7e5204000053039865802BR5925Fernando Carvalho de Oliv6009SAO PAULO62140510CMcrmkQxqk63046E01";
-
-    const handleCopyPix = async () => {
-        await navigator.clipboard.writeText(pixCopyPaste);
-        showSnackbar("Código PIX copiado!");
-    };
 
     return (
         <>
@@ -28,49 +17,10 @@ export const DonateButton = () => {
                 <BiSolidDonateHeart />
             </button>
 
-            <Modal
+            <DonateModal
                 isOpen={donateModalIsOpen}
                 onClose={() => setDonateModalIsOpen(false)}
-            >
-                <div className="donate-modal-content">
-                    <h3>Apoie o TudoAqui 💖</h3>
-
-                    <p>
-                        O TudoAqui é um projeto independente, feito com carinho e foco em
-                        design, liberdade e simplicidade.
-                    </p>
-
-                    <p>
-                        Se essa plataforma te ajuda de alguma forma, considere fazer uma
-                        doação via PIX.
-                    </p>
-
-                    <hr className="custom-hr-secondary" />
-
-                    <div className="pix-area">
-                        <h4>Doe qualquer valor</h4>
-                        <p>Escaneie o QR Code abaixo ou use o PIX copia e cola.</p>
-                        <img
-                            src={DonateQRCode}
-                            alt="QR Code PIX para doação"
-                            className="donate-qrcode"
-                            width={160}
-                            height={160}
-                        />
-
-                        <textarea
-                            readOnly
-                            value={pixCopyPaste}
-                            onClick={(e) => e.currentTarget.select()}
-                            rows={6}
-                        />
-
-                        <button className="btn secondary" onClick={handleCopyPix}>
-                            Copiar código PIX
-                        </button>
-                    </div>
-                </div>
-            </Modal>
+            />
         </>
     );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,16 @@
-export const Footer = () => <footer> <p>Copyright tudoaqui 2025. Feito no Brasil 🇧🇷</p> </footer>
+import { useState } from "react";
+import { DonateModal } from "./DonateModal"
+
+export const Footer = () => {
+    const [donateModalIsOpen, setDonateModalIsOpen] = useState(false);
+
+    return (
+        <>
+            <footer> 
+                <p>Feito com ❤️ por <a href="https://tudoaqui.click/founder" target="_blank" rel="noopener noreferrer">Fernando</a>.</p> 
+                <p className="donate-option" onClick={() => setDonateModalIsOpen(true)}>Apoie este projeto</p>
+            </footer>
+            <DonateModal isOpen={donateModalIsOpen} onClose={() => setDonateModalIsOpen(false)} />
+        </>
+    )
+}
